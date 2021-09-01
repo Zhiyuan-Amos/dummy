55852d3dd353e8284cafb664871476acf3379fc0
Zhi Yuan <yongzhiyuan@u.nus.edu>
2021-09-02 07:12:44 +0800

    Add settings.kts

---
+++ .teamcity/settings.kts
@@ -0,0 +1,18 @@
+import jetbrains.buildServer.configs.kotlin.v2019_2.*
+import jetbrains.buildServer.configs.kotlin.v2019_2.buildSteps.script
+
+version = "2021.1"
+
+project {
+  buildType(HelloWorld)
+}
+
+object HelloWorld: BuildType({
+    name = "Hello world"
+    steps {
+        script {
+            scriptContent = "echo 'Hello world!'"
+        }
+    }
+})
+    
