374ea77cab67e87eb17358b11091036811c415c7
Zhi Yuan <yongzhiyuan@u.nus.edu>
2021-09-08 07:51:14 +0800

    Test: Add Bar Build Configuration back

--- .teamcity/settings.kts
+++ .teamcity/settings.kts
@@ -4,7 +4,7 @@ import jetbrains.buildServer.configs.kotlin.v2019_2.triggers.vcs
 
 version = "2021.1"
 
-val dummies = listOf(Dummy("Foo", "foo"))
+val dummies = listOf(Dummy("Foo", "foo"), Dummy("Bar", "bar"))
 
 project {
    for (dummy in dummies) {
